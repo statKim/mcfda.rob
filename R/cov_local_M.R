de48bfacaffe62c5d5a5666ed81c26f5a21ab38a
statKim <hyunsung1021@gmail.com>
2021-12-16 09:01:29 +0900

    [Add] cov_Mest from robfpca

--- R/cov_local_M.R
+++ R/cov_local_M.R
@@ -2,129 +2,7 @@
 ### Local M-estimator for partially observed functional data
 #################################################################
 
-#' Convert a list to a matrix
-#'
-#' @param X a list containing 2 list (Lt, Ly).
-#'
-#' @return a n x p matrix containing functional trajectories.
-#' n is the number of curves, and p is the number of timepoints.
-#'
-#' @importFrom dplyr %>%
-#'
-#' @export
-list2matrix <- function(X) {
-    Lt <- X$Lt
-    Ly <- X$Ly
-    id <- lapply(1:length(Lt), function(i){ rep(i, length(Lt[[i]]))  })
-
-    # spread data
-    x <- data.frame(id = unlist(id),
-                    y = unlist(Ly),
-                    t = unlist(Lt)) %>%
-        tidyr::spread(key = "t", value = "y")
-    x <- x[, -1] %>%
-        as.matrix
-
-    return(x)
-}
-
-
-#' Get eigenanalysis results
-#'
-#' Get eigenanalysis results including eigenvalues, corresponding eigenvectors, and a proportion of variance explained (PVE)
-#'
-#' @param cov a matrix of covariance matrix
-#' @param grid a vector containing observed time points. (same with dimension of \code{cov})
-#'
-#' @return a list contatining follows:
-#' \item{lambda}{a vector of positive eigenvalues}
-#' \item{phi}{a matrix containing columnwise eigenvectors}
-#' \item{PVE}{a vector containing proportion of variance explained}
-#' \item{cov_psd}{a matrix containing covariance matrix transformed via positive semi-definite}
-#'
-#' @export
-get_eigen <- function(cov, grid) {
-    eig <- eigen(cov, symmetric = T)
-    positive_ind <- which(eig$values > 0)
-    lambda <- eig$values[positive_ind]
-    phi <- matrix(eig$vectors[, positive_ind],
-                  ncol = length(positive_ind))
-    PVE <- cumsum(lambda) / sum(lambda)
-
-    # normalization since discretization technique - from "fdapace" package
-    lambda <- lambda * (grid[2] - grid[1])
-    phi <- apply(phi, 2, function(x) {
-        x <- x / sqrt(fdapace::trapzRcpp(grid, x^2))
-        if ( 0 <= sum(x * 1:length(grid)) )
-            return(x)
-        else
-            return(-x)
-    })
-
-    # fitted covariance which is transformed to positive semi-definite
-    if (length(lambda) > 1) {
-        cov_psd <- phi %*% diag(lambda) %*% t(phi)
-    } else {
-        cov_psd <- lambda * (phi %*% t(phi))
-    }
-
-    return(list(lambda = lambda,
-                phi = phi,
-                PVE = PVE,
-                cov_psd = cov_psd))
-}
-
-
-
-#' Change the sign of eigenvectors to target eigenvectors
-#'
-#' @param eig_vec a vector (n x 1) or matrix (n x q) containing estimated eigenvectors
-#' @param target a vector (n x 1) or matrix (n x q) containing true (or want to convert) eigenvectors
-#'
-#' @return \code{eig_vec} modified to correspond to the sign of \code{target}
-#'
-#' @export
-check_eigen_sign <- function(eig_vec, target) {
-    if (is.matrix(eig_vec) & is.matrix(target)) {
-        ## if inputs are eigenvector matrices
-        eig_vec_dim <- dim(eig_vec)
-        target_dim <- dim(target)
-        if (!isTRUE(all.equal(eig_vec_dim, target_dim))) {
-            stop("The dimensions of 2 eigenvector matrices are not equal.")
-        }
-
-        for (i in 1:eig_vec_dim[2]) {
-            sse_pos <- sum((eig_vec[, i] - target[, i])^2)
-            sse_neg <- sum(((-eig_vec[, i]) - target[, i])^2)
-
-            if (sse_pos > sse_neg) {
-                eig_vec[, i] <- -eig_vec[, i]
-            }
-        }
-    } else if (is.numeric(eig_vec) & is.numeric(target)) {
-        ## if inputs are eigenvectors
-        if (length(eig_vec) != length(target)) {
-            stop("The dimensions of 2 eigenvector matrices are not equal.")
-        }
-
-        sse_pos <- sum((eig_vec - target)^2)
-        sse_neg <- sum(((-eig_vec) - target)^2)
-
-        if (sse_pos > sse_neg) {
-            eig_vec <- -eig_vec
-        }
-    } else {
-        stop("Inputs are not n x q matrices or q-dim vectors. Check the input objects.")
-    }
-
-    return(eig_vec)
-}
-
-
-
-
-
-
+### calculate kernel
 get_kernel <- function(t_vec, t, bw, method = "epan") {
     u <- (t_vec - t) / bw
     kern <- (1-u^2) * (3/4)
@@ -215,7 +93,7 @@ cv.mean_local_M <- function(X,
                             engine = "C++") {
     if (is.list(X)) {
         gr <- sort(unique(unlist(X$Lt)))
-        X <- list2matrix(X)
+        X <- robfpca::list2matrix(X)
     } else {
         gr <- seq(0, 1, length.out = ncol(X))
     }
@@ -545,7 +423,7 @@ cov_local_M <- function(X,
     }
 
     # make positive definite
-    eig <- get_eigen(cov_mat, gr)
+    eig <- robfpca::get_eigen(cov_mat, gr)
     lambda <- eig$lambda
     idx_positive <- which(lambda > 0)
     lambda <- lambda[idx_positive]
@@ -586,7 +464,7 @@ cv.cov_local_M <- function(X,
 
     if (is.list(X)) {
         gr <- sort(unique(unlist(X$Lt)))
-        X <- list2matrix(X)
+        X <- robfpca::list2matrix(X)
     } else {
         gr <- seq(0, 1, length.out = ncol(X))
     }
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 
 This is the robust modification of the R package, [mcfda](https://github.com/linulysses/mcfda), by replacing the local linear smoother to the M-type local linear smoother for mean and variance function.
 
+To use this package, you require an additional package `robfpca`. 
+You can also download using the command `devtools::install_github("statKim/robfpca")`
+    
 This is not complete version, and we stopped this project.
 
 
@@ -10,5 +13,6 @@ This is not complete version, and we stopped this project.
 
 ``` r
 # install.packages("devtools")
+devtools::install_github("statKim/robfpca")    # It should be required.
 devtools::install_github("statKim/mcfda.rob")
 ```
